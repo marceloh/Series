8dcdabfe94a905f6be58952f0125d3900941a393
marcelo <marcelo@contassconsultoria.com.br>
2020-07-30 22:12:36 -0300

    aula 91

--- App.js
+++ App.js
@@ -6,8 +6,25 @@ import LoginScreen from './src/pages/LoginScreen';
 
 const AppNavigator = createStackNavigator({
   'Login': {
-    screen: LoginScreen
+    screen: LoginScreen,
+    navigationOptions: {
+      title: 'Bem vindo!',
+    }
   },
+}, {
+    defaultNavigationOptions: {
+      title: "Series!",
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: '#6ca2f7',
+        borderBottomWidth: 1,
+        borderBottomColor: '#C5C5C5',
+      },
+      headerTitleStyle: {
+        color: 'white',
+        fontSize: 30,
+      }
+  }
 });
 
 const AppContainer = createAppContainer(AppNavigator);

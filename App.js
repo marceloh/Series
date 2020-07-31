ba8a7e8a32fba1e08025d46d5f91d573a2b963d7
marcelo <marcelo@contassconsultoria.com.br>
2020-07-30 21:56:02 -0300

    iniciando aula 91

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { createStackNavigator } from 'react-navigation-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import LoginScreen from './src/pages/LoginScreen';
+
+const AppNavigator = createStackNavigator({
+  'Login': {
+    screen: LoginScreen
   },
 });
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
